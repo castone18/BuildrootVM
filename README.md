27c38f151e4eecf98f101207f7018fa7b7aa9e6c
Chris Stone <christopher.stone@outlook.com>
2014-10-27 16:54:02 -0400

    Modify to use tinycore box from castone18/vagrant-tinycore.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ BuildrootVM
 ===========
 
 A tinycore Linux based virtual machine for embedded Linux development using buildroot on Windows/Linux hosts.
+
+##Usage
+BuildrootVM depends on castone18/vagrant-tinycore on github, which provides the base box that BuildrootVM is built upon. Thus, clone
+castone18/vagrant-tinycore and add the tinycore box to your vagrant boxes. After that, you can edit the Vagrantfile. For instance, you may want to
+change the number of cpus. Then it is just a matter of doing a 'vagrant up'. On Linux you will need 'vagrant up --provider libvirt'.
--- provisioner.sh
+++ provisioner.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+# +----------------------------------------------------------------------------
+# | 
+# |   Copyright (c) 2014 Christopher Stone
+# |   Licensed under GPL v2.0, see LICENSE file.
+# | 
+# |   THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# |   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# |   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# |   DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# |   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# |   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# |   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# |   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# |   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# |   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# |   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# | 
+# +----------------------------------------------------------------------------
+# |
+# | provisioner.sh
+# |
+# |    Vagrant invoked provisioner of BuildrootVM. Modify this file to 
+# | add provisioning such as installing additional packages.
+# |
+# +----------------------------------------------------------------------------
 
 echo "Provisioning BuildrootVM..."
 
@@ -12,7 +37,6 @@ cd /home/tc
 /bin/su tc -c "/usr/bin/tce-load -wi ncurses-utils"
 /bin/su tc -c "/usr/bin/tce-load -wi perl5"
 /bin/su tc -c "/usr/bin/tce-load -wi tar"
-/bin/su tc -c "/usr/bin/tce-load -wi rsync"
 /bin/su tc -c "/usr/bin/tce-load -wi python"
 /bin/su tc -c "/usr/bin/tce-load -wi bc"
 /bin/su tc -c "/usr/bin/tce-load -wi mktemp"
@@ -36,6 +60,6 @@ cat >>/opt/.filetool.lst <<EOF
 EOF
 /bin/su tc -c "echo 'y' | backup"
 
-#/bin/su tc -c "git clone http://git.buildroot.net/git/buildroot.git"
+/bin/su tc -c "git clone http://git.buildroot.net/git/buildroot.git"
 
 echo "Provisioning done."
